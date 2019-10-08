995e426058f7a09eb0d5cbaeac87131e82238271
Juliette M <39768882+JulietteMaire@users.noreply.github.com>
2019-10-08 16:39:09 +0100

    Check comments

--- Urine-patches-detection.R
+++ Urine-patches-detection.R
@@ -3,13 +3,13 @@
 #################################
 ## Adaptable R script for urine patch detection
 
-#Required packages (updates 2017)
+# Required packages (updates 2017)
 require("raster")# version 2.6-7, 11/2017 maintainer Robert J. Hijmans, http://www.rspatial.org/
-require("igraph") #clump function
+require("igraph") # clump function
 require("SDMTools") # PatchStat() and ConnLabel
 
 ##################### extra functions
-#Function 'fill.na' fills the holes inside the detected patches
+# Function 'fill.na' fills the holes inside the detected patches
 fill.na<-function(x,h=5) {
   if(is.na(x)[h]) {
     return(round(mean(x,na.rm=TRUE),0))
@@ -33,16 +33,16 @@ erode_pix <- function(j,x) {
   formaskSieve <- maskpatch
   # assign NA to all clumps whose IDs were identified in excludeID
   formaskSieve[maskpatch %in% excludeID] <- NA
-  #plot(formaskSieve)
+  # plot(formaskSieve)
   formaskSieve
 }
 #Function 'cover_calcul' calculates urine patches' cover in each image
-cover_calcul <- function(j,x) { #j=total surface of the field in m2, x=layer to consider after kmeans and isollation of a patch
-  df<-summary(x)   #df[6] #number of Na;s
-  tot_pix<-nrow(x)*ncol(x) #number of pixels in the layes
+cover_calcul <- function(j,x) { # j=total surface of the field in m2, x=layer to consider after kmeans and isollation of a patch
+  df<-summary(x)   # number of Na;s
+  tot_pix<-nrow(x)*ncol(x) # number of pixels in the layes
   tot_cover_pix<-tot_pix-df[6] # number of pixel detected as part of urine patches
-  resolution<-j/tot_pix #resolution
-  tot_cover_surf<-tot_cover_pix*resolution #coverage of urine patches in m2
+  resolution<-j/tot_pix # image resolution
+  tot_cover_surf<-tot_cover_pix*resolution # coverage of urine patches in m2
   tot_cover_perc<-(tot_cover_surf*100)/j # percentage of area detected as urine patches
   results<-data.frame(tot_pix,tot_cover_pix,resolution,tot_cover_surf,tot_cover_perc)
   colnames(results)<-c("tot_pix","tot_cover_pix","resolution","tot_cover_surf","tot_cover_perc")
@@ -58,7 +58,7 @@ all.images <- list.files(images.path, recursive=TRUE, full.names=TRUE, pattern =
 #	Each image is a 15 x 15 m square of grassland
 #	Each image contents a Red, Green, Blue, NIR+Red channel, NIR and DSM (elevation meters from sea level)
 
-##Kmeans segmentation
+## Kmeans segmentation
 for(i in 1:length(all.images)){
   z<-brick(all.images[i])
   names(z)<-c("red","green","blue","NIR+red","NIR","DSM")
@@ -70,9 +70,10 @@ for(i in 1:length(all.images)){
   # In this example of the script, only the NDVI is considered/processed. 
   # Choose the layer you wish to use here.
   all_z<-(z_multi$NDVI)
+  # Center and/or scale raster data
   all_z_scale<-scale(values((all_z)))
-  set.seed(2) 
   # 'set.seed' set the start point of the clustering to make this process reproducible
+  set.seed(2) 
   
   # Run Cluster number function to get the optimal cluster number, here the cluster optimal number was 4
   km<-kmeans(all_z_scale, center=4,iter.max=500,nstart=1,algorithm = "Lloyd")
@@ -82,14 +83,13 @@ for(i in 1:length(all.images)){
   patch<-calc(kmr, function(x){x[x!=3]<- NA;return(x)}) 
  
   ## Refinement
-  #Applied 'erode_pix' to remove isolated pixels detected between urine patches
-  #Chose 200 pixels  which correspond to approximately 300cm2 if resolution 1.56cm/pixel
+  # Applied 'erode_pix' to remove isolated pixels detected between urine patches
+  # Chose 200 pixels  which correspond to approximately 300cm2 if resolution 1.56cm/pixel
   maskpatch<-erode_pix(200, patch)  
-  #Applied 'fill.na'
-  #to remove gaps inside the urine patches detected
+  # Applied 'fill.na' to remove gaps inside the urine patches detected
   maskpatch_fill<- focal(maskpatch,w=matrix(1,3,3),fun=fill.na,pad=TRUE,na.rm=FALSE)
 
-  ##Visualisation of the results
+  ## Visualisation of the results
   par(mfrow=c(1,4))
   plotRGB(z,r=1, g=2, b=3)
   plot(kmr, legend=FALSE)
@@ -100,7 +100,7 @@ for(i in 1:length(all.images)){
   plotRGB(z,r=1, g=2, b=3)
   plot(maskpatch_fill, col="red", legend=FALSE, add=T) 
 
-  ##Calculation of the resulting covers areas and compilation in a csv file
+  ## Calculation of the resulting covers areas and compilation in a csv file
   df<-cover_calcul(225,maskpatch_fill)
   namefile<-paste("coverage_data_300cm",i,".csv") #specific name for each input (image file)
   write.csv(df, namefile)
